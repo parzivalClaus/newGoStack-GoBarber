5ded08b065326cf91195aba27d06fe834c8c34e5
Claudio Ferreira <claudio@aramacan.local>
2020-12-10 17:06:33 -0300

    Bcryptjs

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -15,7 +15,17 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    return response.json(user);
+    // delete user.password;
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
